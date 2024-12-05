236d9665d44e29fd1c22d0eab6cace610dd71887
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-12-05 17:50:54 +0300

    Create dataclass EntityVertex

--- lib/EntityVertex.py
+++ lib/EntityVertex.py
@@ -10,6 +10,9 @@ class EntityVertex:
         self.name = entity.name
         self.weight = entity.weight
 
+    def __hash__(self):
+        return hash((self.name, self.weight))
+
     def __str__(self):
         return (f"Name: {self.name}"
                 f"Weight: {self.weight}")
