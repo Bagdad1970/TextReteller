7a87b0c65c0fcca1cca50aa1650683398ed09c03
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-12-05 11:24:55 +0300

    Create class EntityVertex
    Instance of EntityVertex stores only name and weight of entity

---
+++ lib/EntityVertex.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from Entity import Entity
+
+@dataclass
+class EntityVertex:
+    name: str
+    weight: float
+
+    def __init__(self, entity: Entity):
+        self.name = entity.name
+        self.weight = entity.weight
+
+    def __str__(self):
+        return (f"Name: {self.name}"
+                f"Weight: {self.weight}")
