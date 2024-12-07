15a457a6752522d7c77cf126674bed10a15153cf
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-12-08 02:56:41 +0300

    (EntityVertex) Add equal method

--- lib/EntityVertex.py
+++ lib/EntityVertex.py
@@ -5,14 +5,20 @@ from Entity import Entity
 class EntityVertex:
     name: str
     weight: float
+    is_nsubj: bool
 
-    def __init__(self, entity: Entity):
+    def __init__(self, *, entity: Entity, is_nsubj: bool = False):
         self.name = entity.name
         self.weight = entity.weight
+        self.is_nsubj = is_nsubj
 
     def __hash__(self):
         return hash((self.name, self.weight))
 
+    def __eq__(self, other):
+        return self.name == other.name and self.weight == other.weight
+
     def __str__(self):
-        return (f"Name: {self.name}"
-                f"Weight: {self.weight}")
+        return (f"Name: {self.name} "
+                f"Weight: {self.weight} "
+                f"Is_nsubj: {self.is_nsubj} ")
