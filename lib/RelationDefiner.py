ec99c59a2f1194bf6006b609fc73089e144459c1
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-12-08 01:07:30 +0300

    Fix: Finds recursive dependent nouns from other nouns

--- lib/RelationDefiner.py
+++ lib/RelationDefiner.py
@@ -1,10 +1,8 @@
 from natasha.doc import DocToken
-from lib.SentenceDict import SentenceDict
 from lib.EntityDict import EntityDict
 from lib.TextParser import TextParser
-from lib.Entity import Entity
 from pymorphy3 import MorphAnalyzer
-from EntityVertex import EntityVertex
+#from EntityVertex import EntityVertex
 
 class RelationDefiner:
     morph = MorphAnalyzer()
@@ -12,10 +10,9 @@ class RelationDefiner:
                             "nmod": 2, "amod": 1, "nsubj:pass": 6
                             }
 
-    def __init__(self, *, text: str, entity_dict: EntityDict, sentence_dict: SentenceDict):
+    def __init__(self, *, text: str, entity_dict: EntityDict):
         self.entity_dict = entity_dict
         self.parsed_text = TextParser.get_parsed_text(text)
-        self.sentence_dict = sentence_dict
 
     @classmethod
     def to_normal_form(cls, doc_token: DocToken):
@@ -55,39 +52,75 @@ class RelationDefiner:
 
         return verbs_dict
 
-    def entities_dependent_from_verbs_in_sentence(self, sent_index: int, tokens: list):
+    def entities_dependent_from_verbs_in_sentence(self, sent_index: int, tokens: list) -> list:
+        """
+        Ищет зависимые от глаголов сущности.
+        :param sent_index: Индекс текущего обрабатываемого предложения
+        :param tokens: Список токенов текущего предложения
+        :return: Список кортежей зависимостей (левый элемент является зависимостью правого)
+        """
         dependency_groups = []
 
         verbs_in_sentence = self.verbs_dependencies_in_sentence(tokens)
         for verb, nouns in verbs_in_sentence.items():
-            print(nouns)
             sorted_by_rels_indexes = self.sort_entity_importance_by_relation(nouns)
 
+            the_most_important_token = self.parsed_text.sents[sent_index].tokens[sorted_by_rels_indexes[0]]
+            if the_most_important_token.rel == 'nsubj':
+                dependency_groups.append((self.to_normal_form(the_most_important_token), 'root'))
+
             for indexes in zip(sorted_by_rels_indexes, sorted_by_rels_indexes[1:]):
-                # добавить обработку nsubj
                 dependency = self.parsed_text.sents[sent_index].tokens[indexes[0]]
                 dependent = self.parsed_text.sents[sent_index].tokens[indexes[1]]
                 dependency_groups.append((self.to_normal_form(dependency), self.to_normal_form(dependent)))
 
         return dependency_groups
 
-    @classmethod
-    def entities_dependent_from_nouns_in_sentence(cls, tokens: list) -> list:
+    def recursive_iter(self, token: DocToken, depth, visited=None):
+        if visited is None:
+            visited = set()
+
+        if token.id in visited:
+            return None
+
+        visited.add(token.id)
+
+        if depth > 3:
+            return None
+        elif 1 <= depth <= 3:
+            if token.pos == 'VERB':
+                return None
+            elif token.pos == 'NOUN':
+                return token
+            else:
+                splitted_head_id = tuple(token.head_id.split('_'))
+                head_token_sent_index, head_token_word_index = int(splitted_head_id[0]) - 1, int(splitted_head_id[1]) - 1
+                head_token = self.parsed_text.sents[head_token_sent_index].tokens[head_token_word_index]
+                depth += 1
+                return self.recursive_iter(head_token, depth, visited)
+        else:
+            splitted_head_id = tuple(token.head_id.split('_'))
+            head_token_sent_index, head_token_word_index = int(splitted_head_id[0]) - 1, int(splitted_head_id[1]) - 1
+            head_token = self.parsed_text.sents[head_token_sent_index].tokens[head_token_word_index]
+            depth += 1
+            return self.recursive_iter(head_token, depth, visited)
+
+    def entities_dependent_from_nouns_in_sentence(self, tokens: list) -> list:
         dependency_groups = []
 
-        for token in tokens:
+        for token in tokens:  # tokens - массив зависимых токенов
             if token.pos == 'NOUN':  # добавить PROPN (?)
                 try:
-                    dependency_index = cls.id_to_index(token.head_id)
-                    dependency = tokens[dependency_index]
-                    if dependency.pos == 'NOUN':
-                        dependency_groups.append((cls.morph.parse(token.text)[0].normal_form, cls.morph.parse(dependency.text)[0].normal_form))
+                    # dependency - зависимость token
+                    dependent_token = self.recursive_iter(token, 0)
+                    if dependent_token is not None:
+                        dependency_groups.append((self.morph.parse(dependent_token.text)[0].normal_form, self.morph.parse(token.text)[0].normal_form))
                 except (IndexError, ValueError):
                     pass
 
         return dependency_groups
 
-    def dependencies_between_entities(self) -> list:
+    def relations_between_entities(self) -> list:
         total_dependents = []
         for sent_index, sentence in enumerate(self.parsed_text.sents):
             tokens = sentence.tokens
@@ -104,4 +137,4 @@ class RelationDefiner:
         # будем хранить по индексу массива список всех имен сущностей, которые там встречаются
         # по relation определим какую роль они играют в предложении
         # в соответствии с этим выделим слабосвязанные и сильносвязанные сущности
-        return self.dependencies_between_entities()
+        return self.relations_between_entities()
