d9cf50e6f3a388a937f4f477e8ab859185f49a1d
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-12-10 02:36:02 +0300

    ImportanceCounter: Change calculation entity weight formula

--- lib/ImportanceCounter.py
+++ lib/ImportanceCounter.py
@@ -1,14 +1,26 @@
 from lib.EntityDict import EntityDict
-from lib.TextSplitter import TextSplitter
 from lib.Config import Config
+from lib.Entity import Entity
+from lib.TextParser import TextParser
 
-class ImportanceCounter:
-    def __init__(self, text, entity_dict: EntityDict):
-        self.metrics = Config.load_config('entity_metrics.json')
+
+class ImportanceCounter(Config):
+    metrics = Config.load_config('entity_metrics.json')
+
+    def __init__(self, entity_dict: EntityDict):
         self.entity_dict = entity_dict
-        self.tokenized_text = TextSplitter(text).split_text_on_words()
+        self.parsed_text = TextParser.get_parsed_text()
+
+    @staticmethod
+    def count_number_of_different_sentences(entity: Entity):
+        return len(set(sent_index for sent_index in entity.sentence_word_indexes.keys()))
+
+    def weight_for_entity(self, entity) -> float:
+        relation_sum = sum([self.metrics[relation] for relation in entity.relations])
+        sents_with_entity = self.count_number_of_different_sentences(entity)
+        sents_in_text = len(self.parsed_text.sents)
+        return (relation_sum * sents_with_entity) / (sents_in_text * (1 + relation_sum))
 
     def find_weights(self) -> None:
         for entity in self.entity_dict:
-            summary_weight = sum([self.metrics[relation] for relation in entity.relations])
-            entity.weight = summary_weight
+            entity.weight_entity = self.weight_for_entity(entity)
