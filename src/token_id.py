b7916ddb67411007834ed8f216a990da3f1725af
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-03-28 20:32:49 +0300

    Syntax: TokenID

--- src/token_id.py
+++ src/token_id.py
@@ -1,14 +1,14 @@
 class TokenID:
 
     @staticmethod
-    def id_to_sentence_index(id: str) -> int:
+    def get_sentence_index(id: str) -> int:
         return int(id.split('_')[0]) - 1
 
     @staticmethod
-    def id_to_word_index(id: str) -> int:
+    def get_word_index(id: str) -> int:
         return int(id.split('_')[1]) - 1
 
     @staticmethod
-    def id_to_tuple_index(id: str) -> tuple[int, int]:
+    def get_tuple_index(id: str) -> tuple[int, int]:
         splitted_id = id.split('_')
         return int(splitted_id[0]) - 1, int(splitted_id[1]) - 1
