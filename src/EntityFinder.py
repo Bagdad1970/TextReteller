515bd831fc6b98f678ca323839c8f58e217652bd
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-03-28 20:29:03 +0300

    Refactoring: Replace methods from SimpleEntityFinder to EntityFinder

--- src/EntityFinder.py
+++ src/EntityFinder.py
@@ -1,11 +1,30 @@
-from src.SimpleEntityFinder import SimpleEntityFinder
+from src.Entity import Entity
+from src.EntityDict import EntityDict
 from src.TextParser import TextParser
 
 
 class EntityFinder:
     def __init__(self, text_parser: TextParser):
-        parsed_text = text_parser.get_parsed_text()
-        self.simple_entity_finder = SimpleEntityFinder(parsed_text)
+        self.parsed_text = text_parser.get_parsed_text()
 
-    def find_entities(self):
-        return self.simple_entity_finder.find_entities()
+    def find_simple_entities(self) -> EntityDict:
+        """
+        Finds simple entities in text. Simple entity is unnamed none like cat, door etc.
+        Entities like Smith, Brooklin street are not simple entities, there are named entities
+        :return: Instance of EntityDict, that stores simple text entities
+        """
+        entity_dict = EntityDict()
+        # найдем root (сказуемое)
+        # то слово типа nsubj, obj, ..., которое с ним связано через head_id, явл. подлежащим
+        # после этого также проверяется к какой части речи принадлежит слово
+        for sent_index, sentence in enumerate(self.parsed_text.sents):
+            for word_index, word in enumerate(sentence.tokens):
+                if word.pos == 'NOUN':
+                    # продолжаем читать слова, пока не закончится flat::... т.к. может быть именованой сущностью
+                    entity = Entity(name=word.text)
+                    entity.add_index(sent_index, word_index)
+                    entity.add_relation(word.rel)
+
+                    entity_dict.add_entity(entity)
+
+        return entity_dict
