7f666783f2634c0ed339252c7e3b63ed8df5a1cb
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-03-30 14:07:35 +0300

    Create tests for RelationDefiner

--- src/relation_definer.py
+++ src/relation_definer.py
@@ -1,42 +1,48 @@
-from natasha.doc import DocToken
+from natasha.doc import DocToken, Doc
 from src.entity_dict import EntityDict
 from src.word_normalizer import WordNormalizer
 from src.token_id import TokenID
 
 class RelationDefiner(TokenID, WordNormalizer):
-    relations_importance = {"nsubj": 6, "obj": 5, "iobj": 4, "obl": 3,
+    relations_priority = {"nsubj": 6, "obj": 5, "iobj": 4, "obl": 3,
                             "nmod": 2, "amod": 1, "nsubj:pass": 6
-                            }
+                          }
 
-    def __init__(self, parsed_text, entity_dict: EntityDict):
-        self.parsed_text = parsed_text
-        self.entity_dict = entity_dict
+    def __init__(self, parsed_text: Doc, entity_dict: EntityDict):
+        self.__parsed_text = parsed_text
+        self.__entity_dict = entity_dict
 
     @classmethod
     def sort_entity_importance_by_relation(cls, tokens: list[DocToken]) -> list:
         """
-        Сортирует токены по важности rels в предложении, у которых head_id явл. одинаковым глаголом
+        Сортирует токены по приоритету rels в предложении, у которых head_id явл. одинаковым глаголом
         :param tokens: Токены одинакового глагола
         :return: Список индексов отсортированных по rels
         """
-        token_rels = [(cls.get_word_index(token.id), cls.relations_importance[token.rel]) for token in tokens]
-        return [token[0] for token in sorted(token_rels, key=lambda x: x[1], reverse=True)]
+        token_rels = [{'id': cls.get_word_index(token.id), 'rel': cls.relations_priority[token.rel]} for token in tokens]
+        return [token.get('id') for token in sorted(token_rels, key=lambda x: x.get('rel'), reverse=True)]
 
     @classmethod
-    def verbs_dependencies_in_sentence(cls, tokens: list) -> dict:
+    def nouns_depends_from_verbs_in_sentence(cls, tokens: list) -> dict:
+        """
+        Finds all verbs and nouns that depend on them in sentence
+        :param tokens: all tokens in sentence
+        :return: dictionary of verbs and nouns
+        """
         verbs_dict = dict()
 
         for token in tokens:
             if token.pos == 'NOUN':  # добавить PROPN (?)
                 try:
-                    dependency_index = cls.get_word_index(token.head_id)
+                    dependency_index = TokenID.get_word_index(token.head_id)
                     dependency = tokens[dependency_index]
                     if dependency.pos == 'VERB':
-                        verb_text = dependency.text
-                        if verb_text not in verbs_dict:
-                            verbs_dict[verb_text] = [token]
-                        else:
-                            verbs_dict[verb_text].append(token)
+                        if 'acl' not in dependency.rel:  # consider only verbs
+                            verb_word = dependency.text
+                            if verb_word not in verbs_dict:
+                                verbs_dict[verb_word] = [ token ]
+                            else:
+                                verbs_dict[verb_word].append(token)
                 except (IndexError, ValueError):
                     pass
 
@@ -51,18 +57,23 @@ class RelationDefiner(TokenID, WordNormalizer):
         """
         dependency_groups = []
 
-        verbs_in_sentence = self.verbs_dependencies_in_sentence(tokens)
-        for verb, nouns in verbs_in_sentence.items():
+        verb_nouns_in_sentence = self.nouns_depends_from_verbs_in_sentence(tokens)
+        for verb, nouns in verb_nouns_in_sentence.items():
             sorted_by_rels_indexes = self.sort_entity_importance_by_relation(nouns)
 
-            for indexes in zip(sorted_by_rels_indexes, sorted_by_rels_indexes[1:]):
-                dependency, dependent = self.parsed_text.sents[sent_index].tokens[indexes[0]], self.parsed_text.sents[sent_index].tokens[indexes[1]]
+            associated_noun_pairs = zip(sorted_by_rels_indexes, sorted_by_rels_indexes[1:])  # arranges the nouns in order of coherence like [ (nsubj, obj), (obj, ) ]
+            for indexes in associated_noun_pairs:
+                current_sentence_tokens = self.__parsed_text.sents[sent_index].tokens
 
-                dependency_groups.append((self.name_to_normal_form(dependency.text), self.name_to_normal_form(dependent.text)))
+                dependency, dependent = current_sentence_tokens[indexes[0]], current_sentence_tokens[indexes[1]]
+                print(dependency, dependent)
+                dependency_groups.append( (WordNormalizer.word_to_normal_form(dependency.text),
+                                          WordNormalizer.word_to_normal_form(dependent.text))
+                                          )
 
         return dependency_groups
 
-    def recursive_iter(self, token: DocToken, depth: int, visited=None):
+    def __recursive_iter(self, token: DocToken, depth: int, visited=None):
         if visited is None:
             visited = set()
 
@@ -81,15 +92,15 @@ class RelationDefiner(TokenID, WordNormalizer):
             else:
                 splitted_head_id = tuple(token.head_id.split('_'))
                 head_token_sent_index, head_token_word_index = int(splitted_head_id[0]) - 1, int(splitted_head_id[1]) - 1
-                head_token = self.parsed_text.sents[head_token_sent_index].tokens[head_token_word_index]
+                head_token = self.__parsed_text.sents[head_token_sent_index].tokens[head_token_word_index]
                 depth += 1
-                return self.recursive_iter(head_token, depth, visited)
+                return self.__recursive_iter(head_token, depth, visited)
         else:
             splitted_head_id = tuple(token.head_id.split('_'))
             head_token_sent_index, head_token_word_index = int(splitted_head_id[0]) - 1, int(splitted_head_id[1]) - 1
-            head_token = self.parsed_text.sents[head_token_sent_index].tokens[head_token_word_index]
+            head_token = self.__parsed_text.sents[head_token_sent_index].tokens[head_token_word_index]
             depth += 1
-            return self.recursive_iter(head_token, depth, visited)
+            return self.__recursive_iter(head_token, depth, visited)
 
     def entities_dependent_from_nouns_in_sentence(self, tokens: list) -> list:
         dependency_groups = []
@@ -98,9 +109,9 @@ class RelationDefiner(TokenID, WordNormalizer):
             if token.pos == 'NOUN':  # добавить PROPN (?)
                 try:
                     # dependency - зависимость token
-                    dependent_token = self.recursive_iter(token, 0)
+                    dependent_token = self.__recursive_iter(token, 0)
                     if dependent_token is not None:
-                        dependency_groups.append((self.morph.parse(dependent_token.text)[0].normal_form, self.morph.parse(token.text)[0].normal_form))
+                        dependency_groups.append((self.morph.parse(dependent_token.text)[0].normal_form, self.morph.parse(token.sentence)[0].normal_form))
                 except (IndexError, ValueError):
                     pass
 
@@ -108,17 +119,17 @@ class RelationDefiner(TokenID, WordNormalizer):
 
     def transform_to_entity_vertexes(self, couple_entity_names: tuple[str, str]) -> tuple:
         dependency, dependent = couple_entity_names
-        dependency_entity_main, dependent_entity_main = (self.entity_dict[dependency].separate_entity_main(),
-                                                             self.entity_dict[dependent].separate_entity_main())
+        dependency_entity_main, dependent_entity_main = (self.__entity_dict[dependency].separate_entity_main(),
+                                                             self.__entity_dict[dependent].separate_entity_main())
 
         return dependency_entity_main, dependent_entity_main
 
     def relations_of_entities(self) -> list:
         # будем хранить по индексу массива список всех имен сущностей, которые там встречаются
         # по relation определим какую роль они играют в предложении
-        # в соответствии с этим выделим слабосвязанные и сильносвязанные сущности
+        # в соответствии с этим выделим слабосвязанные и сильно-связанные сущности
         total_dependents = []
-        for sent_index, sentence in enumerate(self.parsed_text.sents):
+        for sent_index, sentence in enumerate(self.__parsed_text.sents):
             tokens = sentence.tokens
 
             dependent_from_verbs = self.entities_dependent_from_verbs_in_sentence(sent_index, tokens)
@@ -129,4 +140,3 @@ class RelationDefiner(TokenID, WordNormalizer):
                 total_dependents.append(couple_entity_vertexes)
 
         return total_dependents
-
--- tests/test_finding_entities.py
+++ tests/test_finding_entities.py
@@ -7,16 +7,16 @@ from src.entity import Entity
 
 
 def test_finding_simple_entities():
-    text_parser = TextParser("Кошки не едят овощные овощи. Собаки едят мясное мясо.")
-    entity_finder = EntityFinder(text_parser)
+    text_parser = TextParser("Кошки не едят овощные овощи. Собаки едят мясное мясо. Кошка играется с собакой.")
+    entity_finder = EntityFinder(text_parser.get_parsed_text())
 
     sut = entity_finder.find_simple_entities()
 
     assert sut == EntityDict.create_from_list(
         [
-            Entity(name="кошка", sentence_word_indexes={0: [0]}, relations=["nsubj"]),
+            Entity(name="кошка", sentence_word_indexes={0: [0], 2: [0]}, relations=["nsubj", "nsubj"]),
             Entity(name="овощ", sentence_word_indexes={0: [4]}, relations=["obj"]),
-            Entity(name="собака", sentence_word_indexes={1: [0]}, relations=["nsubj"]),
+            Entity(name="собака", sentence_word_indexes={1: [0], 2: [3]}, relations=["nsubj", "obj"]),
             Entity(name="мясо", sentence_word_indexes={1: [3]}, relations=["obj"]),
         ]
     )
