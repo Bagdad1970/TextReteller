48f5ff79454d2ad342b836a7e7fd19f72773c712
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-03-30 20:58:28 +0300

    Refactoring: instancemethod TextParser.parse_text() becomes classmethod

--- src/text_parser.py
+++ src/text_parser.py
@@ -10,17 +10,18 @@ class TextParser:
     morph_tagger = NewsMorphTagger(embedding)
     syntax_parser = NewsSyntaxParser(embedding)
 
-    def __init__(self, text):
+    def __init__(self, text: str):
         self.parsed_text = self.parse_text(text)
 
-    def parse_text(self, text: str):
+    @classmethod
+    def parse_text(cls, text: str):
         if text == '':
             raise ValueError("Invalid text")
 
         doc = Doc(text)
-        doc.segment(self.segmenter)
-        doc.tag_morph(self.morph_tagger)
-        doc.parse_syntax(self.syntax_parser)
+        doc.segment(cls.segmenter)
+        doc.tag_morph(cls.morph_tagger)
+        doc.parse_syntax(cls.syntax_parser)
         return doc
 
     def get_parsed_text(self) -> Doc:
