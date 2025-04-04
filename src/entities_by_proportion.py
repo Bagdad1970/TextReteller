148f23ef6e54e153d5f38401bd2ae356e52395cd
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-04-04 12:58:26 +0300

    Rename week_entities to entities_by_proportion

--- src/entities_by_proportion.py
+++ src/entities_by_proportion.py
@@ -2,14 +2,14 @@ from src.entity_dict import EntityDict
 
 
 def get_week_entities(entity_dict, proportion: float) -> EntityDict:
-    week_entities = sorted(entity_dict, key=lambda entity: entity.total_weight)[
+    week_entities = sorted(entity_dict.values(), key=lambda entity: entity.total_weight)[
         0 : int(len(entity_dict) * proportion)
     ]
-    return EntityDict.create_from_list(week_entities)
+    return EntityDict(week_entities)
 
 
 def get_strong_entities(entity_dict, proportion: float) -> EntityDict:
-    strong_entities = sorted(entity_dict, key=lambda entity: entity.total_weight)[
+    strong_entities = sorted(entity_dict.values(), key=lambda entity: entity.total_weight)[
         int(len(entity_dict) * proportion) :
     ]
-    return EntityDict.create_from_list(strong_entities)
+    return EntityDict(strong_entities)
